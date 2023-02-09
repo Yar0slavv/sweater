6606e57768a09ddf4811e74f1aef34685442e607
Yaroslav Martryniuk <45628784+Yar0slavv@users.noreply.github.com>
2023-02-10 00:26:08 +0200

    clean code

--- src/main/java/com/mysn/sweater/GreetingController.java
+++ src/main/java/com/mysn/sweater/GreetingController.java
@@ -4,12 +4,10 @@ import com.mysn.sweater.domain.Message;
 import com.mysn.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.SecureRandom;
 import java.util.Map;
 
 @Controller
--- src/main/java/com/mysn/sweater/SweaterApplication.java
+++ src/main/java/com/mysn/sweater/SweaterApplication.java
@@ -2,7 +2,6 @@ package com.mysn.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
